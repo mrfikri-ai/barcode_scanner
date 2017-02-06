be37db80efefc2e8e8e59f0744135c77a3befcd8
Muhamad Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2017-02-06 20:02:48 +0900

    Create Information.java

---
+++ main/java/Information.java
@@ -0,0 +1,18 @@
+package com.example.newstudent.barcode_v13;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by new student on 2/5/2017.
+ */
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information);
+
+    }
+}
