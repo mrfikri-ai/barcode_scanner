79b95e3067c77e2bca6f7ed727a27e76d6a4ff20
Muhamad Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2017-07-12 17:54:31 +0900

    Create Food.java

---
+++ main/res/java/Food.java
@@ -0,0 +1,17 @@
+package com.example.newstudent.barcode_v12;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by new student on 2/1/2017.
+ */
+
+public class Food extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food);
+    }
+}
