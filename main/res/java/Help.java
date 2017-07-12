40864f4522afd40817f10c1cd6921210a3032eff
Muhamad Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2017-07-12 17:55:04 +0900

    Create Help.java

---
+++ main/res/java/Help.java
@@ -0,0 +1,18 @@
+package com.example.newstudent.barcode_v12;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by new student on 2/1/2017.
+ */
+
+public class Help extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
